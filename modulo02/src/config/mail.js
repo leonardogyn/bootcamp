a836000e993313a69bc189619898e2bded078886
Leonardo <leonardogynn@gmail.com>
2020-03-21 06:51:57 -0300

    Removendo arquivos desnecessários Inclusão:  bee-queue  Mail  Jobs

---
+++ modulo02/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'dcf47e12578ecc',
+    pass: '4edbcb31431144',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreplay@gobarber.com.br>',
+  },
+};
