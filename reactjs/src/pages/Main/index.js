188f8e095ede58deb44ab32d211ac54cfd4a863c
Leonardo <leonardogynn@gmail.com>
2020-03-26 04:18:25 -0300

    Busca de dados da API do github, styled components, LocalStorage, Rotas, PropTypes, exibir dados da API

--- reactjs/src/pages/Main/index.js
+++ reactjs/src/pages/Main/index.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 
+import { Link } from 'react-router-dom';
+
 import api from '../../services/api';
 
-import { Container, Form, List, SubmitButton } from './styles';
+import Container from '../../component/Conteiner';
+
+import { Form, List, SubmitButton } from './styles';
 
 export default class Main extends Component {
   state = {
@@ -11,6 +15,26 @@ export default class Main extends Component {
     repositories: [],
     loading: false,
   };
+
+  /** Carregar dados do localStorage */
+  componentDidMount() {
+    const repositories = localStorage.getItem('repositories');
+
+    if (repositories) {
+      this.setState({
+        repositories: JSON.parse(repositories),
+      });
+    }
+  }
+
+  /** Salvar dados no localStorage */
+  componentDidUpdate(_, prevState) {
+    const { repositories } = this.state;
+
+    if (prevState.repositories !== repositories) {
+      localStorage.setItem('repositories', JSON.stringify(repositories));
+    }
+  }
 
   handleInputChange = (e) => {
     this.setState({ newRepo: e.target.value });
@@ -66,7 +90,9 @@ export default class Main extends Component {
           {repositories.map((repository) => (
             <li key={repository.name}>
               <span>{repository.name}</span>
-              <a href="#">Detalhes</a>
+              <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
+                Detalhes
+              </Link>
             </li>
           ))}
         </List>
