52476fa874a91f95976d1b3eb6d94df3940f147b
Leonardo Nunes <leonardogynn@gmail.com>
2020-06-14 13:48:21 -0300

    Criando o styled-components global com background svg

--- loja/src/App.js
+++ loja/src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 import { BrowserRouter } from 'react-router-dom';
 
+import GlobalStyle from './styles/global';
+
 import Routes from './routes';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes />
+      <GlobalStyle />
     </BrowserRouter>
   );
 }
